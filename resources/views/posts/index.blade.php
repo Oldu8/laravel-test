1e9c586f413b8838e60ff5839025480914d23013
oleh-runzy <dudkoleg@gmail.com>
2024-02-25 11:54:42 +0200

    add scss, and main layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
-      <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="antialiased">
-    <section class="max-w-md mx-auto my-2">
-        <header class="flex justify-between">
-            <a href="/posts">Home</a>
-            <a href="/posts/create/">Create Post</a>
-        </header>
-        <h1 class="text-3xl font-bold underline text-center">My posts</h1>
+<x-layout.main title="My posts">
         <div class='ml-4 mt-4'>
             @foreach ($posts as $post)
             <li class='font-semibold text-gray-600 mb-2'>
@@ -28,6 +10,4 @@
             </li>
             @endforeach
         </div>
-    </section>
-</body>
-</html>
+</x-layout.main>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
-      <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="antialiased py-10">
-        <section class="max-w-md mx-auto my-2">
-        <header class="flex justify-between py-8">
-            <a href="/posts">Home</a>
-            <a href="/posts/create/">Create Post</a>
-        </header>
-        <h1 class="text-3xl font-bold underline text-center">Post {{ $post->id }}: {{ $post->title }}</h1>
+<x-layout.main title="My posts" title="{{ $post->title }}" h1="Post {{ $post->id }}: {{ $post->title }}">
         <div class='ml-4 mt-4 font-semibold text-gray-600 mb-2'>
             <p>Description: {{ $post->description }}</p>
         </div>
@@ -27,6 +9,4 @@
                 <button type="submit" class="bg-red-500 text-white rounded p-2">Delete</button>
             </form>
         </div>
-    </section>
-</body>
-</html>
+</x-layout.main>
